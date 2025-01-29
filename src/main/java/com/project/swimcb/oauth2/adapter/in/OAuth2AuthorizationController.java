e5c2b2ba7ceda995f183666329a696e7c87e7a1d
pu1et <pu1etproof@gmail.com>
2025-01-29 21:02:17 +0900

    feature: 카카오 회원가입/로그인 목업 API 생성

---
+++ src/main/java/com/project/swimcb/oauth2/adapter/in/OAuth2AuthorizationController.java
@@ -0,0 +1,20 @@
+package com.project.swimcb.oauth2.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Tag(name = "회원")
+@RestController
+@RequestMapping("/oauth2/authorization/kakao")
+public class OAuth2AuthorizationController {
+
+  @Operation(summary = "카카오 회원가입/로그인")
+  @GetMapping
+  public RedirectView kakao() {
+    return new RedirectView("카카오 회원가입/로그인 URL");
+  }
+}
