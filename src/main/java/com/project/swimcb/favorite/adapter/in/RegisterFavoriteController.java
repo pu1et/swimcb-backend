587898eb6b2ecc74009e7eaa94fe112551ea09cc
pu1et <pu1etproof@gmail.com>
2025-01-29 21:38:35 +0900

    feat: 즐겨찾기 등록 목업 API 생성

---
+++ src/main/java/com/project/swimcb/favorite/adapter/in/RegisterFavoriteController.java
@@ -0,0 +1,20 @@
+package com.project.swimcb.favorite.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "즐겨찾기")
+@RestController
+@RequestMapping("/favorite")
+public class RegisterFavoriteController {
+
+  @Operation(summary = "즐겨찾기 등록")
+  @PostMapping
+  public void registerFavorite(@RequestBody RegisterFavoriteRequest request) {
+
+  }
+}
