be195321550f4c226bd10265d0a9d940e878493a
pu1et <pu1etproof@gmail.com>
2025-01-29 20:14:22 +0900

    init

---
+++ src/main/java/com/project/swimcb/SwimcbApplication.java
@@ -0,0 +1,13 @@
+package com.project.swimcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimcbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/swimcb/SwimcbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.swimcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
