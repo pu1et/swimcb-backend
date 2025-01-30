48edd2f94a3179df22a8fd7feb1daac43b368747
pu1et <pu1etproof@gmail.com>
2025-01-30 21:59:40 +0900

    refactor: 수영 클래스 조회 정렬 조건 삭제

--- src/main/java/com/project/swimcb/swimmingpool/domain/enums/Sort.java
+++ src/main/java/com/project/swimcb/swimmingpool/domain/enums/Sort.java
@@ -5,13 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Sort {
   DISTANCE_ASC("거리 가까운 순"),
-  PRICE_ASC("낮은 가격순"),
-  PRICE_DESC("높은 가격순"),
-  REGISTRATION_START_ASC("신청 시작일 빠른 순"),
-  TIME_ASC("빠른 시간대 순"),
-  TIME_DESC("늦은 시간대 순"),
-  STUDENT_COUNT_DESC("많은 수강 인원 순"),
-  STUDENT_COUNT_ASC("적은 수강 인원 순");
+  PRICE_ASC("낮은 가격순");
 
   private final String name;
 }
