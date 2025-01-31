7746a46de82cd1f140a17e0bc9838945cadf834f
pu1et <pu1etproof@gmail.com>
2025-01-31 10:27:50 +0900

    feat: 자유 수영 예약 목업 API 생성

---
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveFreeSwimmingController.java
@@ -0,0 +1,18 @@
+package com.project.swimcb.swimmingpool.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "자유수영")
+@RestController
+@RequestMapping("/api/free-swimming/{freeSwimmingId}/reservations")
+public class ReserveFreeSwimmingController {
+
+  @Operation(summary = "자유수영 예약")
+  @PostMapping
+  public void reserveFreeSwimming() {
+  }
+}
