9a0e79d7fead7dc724fbd440c42d85e8c2c5cf72
pu1et <pu1etproof@gmail.com>
2025-01-31 10:28:23 +0900

    refactor: 자유수영 예약 정보 조회 Swagger Tag 변경

--- src/main/java/com/project/swimcb/swimmingpool/adapter/in/FindFreeSwimmingReservationInfoController.java
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/FindFreeSwimmingReservationInfoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "클래스")
+@Tag(name = "자유수영")
 @RestController
 @RequestMapping("/api/free-swimming/{freeSwimmingId}/reservations")
 public class FindFreeSwimmingReservationInfoController {
