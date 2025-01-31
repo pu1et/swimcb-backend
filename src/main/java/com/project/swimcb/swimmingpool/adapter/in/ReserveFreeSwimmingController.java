b2ac77c44b6879b54f37dd36a9d76546f6af6683
pu1et <pu1etproof@gmail.com>
2025-01-31 10:29:31 +0900

    refactor: 예약 API에 PathVariable 추가

--- src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveFreeSwimmingController.java
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveFreeSwimmingController.java
@@ -2,6 +2,7 @@ package com.project.swimcb.swimmingpool.adapter.in;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ public class ReserveFreeSwimmingController {
 
   @Operation(summary = "자유수영 예약")
   @PostMapping
-  public void reserveFreeSwimming() {
+  public void reserveFreeSwimming(
+      @PathVariable(value = "freeSwimmingId") long freeSwimmingId
+  ) {
   }
 }
--- src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveSwimmingClassController.java
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveSwimmingClassController.java
@@ -2,6 +2,7 @@ package com.project.swimcb.swimmingpool.adapter.in;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ public class ReserveSwimmingClassController {
 
   @Operation(summary = "수영 클래스 예약")
   @PostMapping
-  public void reserveSwimmingClass() {
+  public void reserveSwimmingClass(
+      @PathVariable(value = "swimmingClassId") long swimmingClassId
+  ) {
   }
 }
