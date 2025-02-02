cdb47fc350451a88a4ef2ffcaed3657e82685cb7
pu1et <pu1etproof@gmail.com>
2025-02-02 18:05:20 +0900

    feat: 공지사항 리스트 조회 목업 API 생성

---
+++ src/main/java/com/project/swimcb/notice/domain/Notice.java
@@ -0,0 +1,16 @@
+package com.project.swimcb.notice.domain;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+public record Notice(
+    long noticeId,
+    @NonNull String title,
+    @NonNull String createdBy,
+    @NonNull LocalDate createdAt,
+    boolean isVisible
+) {
+
+}
