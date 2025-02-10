1fc43a73f67a445174d7156d4c1c836490d55b1c
pu1et <pu1etproof@gmail.com>
2025-02-10 10:42:59 +0900

    refactor: 공지사항 상세 조회 서비스 적용 및 Controller test 추가

--- src/main/java/com/project/swimcb/notice/adapter/in/FindNoticeDetailController.java
+++ src/main/java/com/project/swimcb/notice/adapter/in/FindNoticeDetailController.java
@@ -3,8 +3,6 @@ package com.project.swimcb.notice.adapter.in;
 import com.project.swimcb.notice.application.in.FindNoticeDetailUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDate;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,13 +22,6 @@ public class FindNoticeDetailController {
   public FindNoticeDetailResponse findNotices(
       @PathVariable(value = "noticeId") long noticeId
   ) {
-    return FindNoticeDetailResponse.builder()
-        .createdBy("운영자")
-        .title("공지사항 제목1")
-        .content("공지사항 내용1")
-        .imageUrls(List.of("https://ibb.co/bjGKF8WV", "https://ibb.co/bjGKF8WV"))
-        .isVisible(true)
-        .createdAt(LocalDate.of(2025, 1, 1))
-        .build();
+    return useCase.findDetail(noticeId);
   }
 }
---
+++ src/test/java/com/project/swimcb/notice/adapter/in/FindNoticeDetailControllerTest.java
@@ -0,0 +1,86 @@
+package com.project.swimcb.notice.adapter.in;
+
+import static java.time.LocalDateTime.MIN;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.swimcb.config.security.SecurityConfig;
+import com.project.swimcb.notice.application.in.FindNoticeDetailUseCase;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(FindNoticeDetailController.class)
+@Import(SecurityConfig.class)
+class FindNoticeDetailControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockitoBean
+  private FindNoticeDetailUseCase useCase;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private static final String path = "/api/notices/{noticeId}";
+
+  @Test
+  @DisplayName("공지사항 ID로 상세 조회 성공")
+  void shouldReturnNoticeDetailWhenNoticeExists() throws Exception {
+    // given
+    val noticeId = 1L;
+    val response = FindNoticeDetailResponseFactory.create();
+
+    when(useCase.findDetail(anyLong())).thenReturn(response);
+    // when
+    // then
+    mockMvc.perform(get(path, noticeId))
+        .andExpect(status().isOk())
+        .andExpect(content().json(objectMapper.writeValueAsString(response)));
+
+    verify(useCase, only()).findDetail(noticeId);
+  }
+
+  @Test
+  @DisplayName("존재하지 않는 공지사항 조회 시 예외 발생")
+  void shouldReturn404WhenNoticeNotFound() throws Exception {
+    // given
+    val noticeId = 1L;
+
+    when(useCase.findDetail(anyLong())).thenThrow(NoSuchElementException.class);
+    // when
+    // then
+    mockMvc.perform(get(path, noticeId))
+        .andExpect(status().isNotFound());
+
+    verify(useCase, only()).findDetail(noticeId);
+  }
+
+  private static class FindNoticeDetailResponseFactory {
+
+    private static FindNoticeDetailResponse create() {
+      return FindNoticeDetailResponse.builder()
+          .createdBy("createdBy")
+          .title("title")
+          .content("content")
+          .imageUrls(List.of("image1", "image2"))
+          .isVisible(true)
+          .createdAt(MIN.toLocalDate())
+          .build();
+    }
+  }
+}
