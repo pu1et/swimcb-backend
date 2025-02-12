0225b26d48e57dc989b608874a32f4ecf5cb7b14
pu1et <pu1etproof@gmail.com>
2025-02-12 18:49:15 +0900

    feat: 결제 승인 처리 목업 API 생성

---
+++ src/main/java/com/project/swimcb/bo/reservation/adapter/in/BoApprovePaymentController.java
@@ -0,0 +1,20 @@
+package com.project.swimcb.bo.reservation.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "BO")
+@RestController
+@RequestMapping("/api/bo/payments/{paymentId}/approve")
+public class BoApprovePaymentController {
+
+  @Operation(summary = "결제 승인 처리")
+  @PostMapping
+  public void approvePayment(@PathVariable(value = "paymentId") long paymentId) {
+
+  }
+}
