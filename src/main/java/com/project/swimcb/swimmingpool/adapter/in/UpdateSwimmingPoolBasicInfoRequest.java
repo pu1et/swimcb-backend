fdc11327aeee460f957297b8b7a7e2073dc0f082
pu1et <pu1etproof@gmail.com>
2025-02-12 18:58:01 +0900

    refactor: 수영장 기본 정보 조회 request Swagger 설명 추가

--- src/main/java/com/project/swimcb/swimmingpool/adapter/in/UpdateSwimmingPoolBasicInfoRequest.java
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/UpdateSwimmingPoolBasicInfoRequest.java
@@ -1,24 +1,44 @@
 package com.project.swimcb.swimmingpool.adapter.in;
 
 import com.project.swimcb.swimmingpool.domain.UpdateSwimmingPoolBasicInfoCommand;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "BO 수영장 기본 정보 수정 request")
 public record UpdateSwimmingPoolBasicInfoRequest(
+
+    @Schema(description = "수영장 이름", example = "올림픽수영장")
     String name,
+
+    @Schema(description = "전화번호", example = "02-1234-5678")
     String phone,
+
+    @Schema(description = "주소", example = "서울시 강남구 언주로")
     String address,
-    Integer newEnrollmentStartDate,
-    Integer newEnrollmentEndDate,
+
+    @Schema(description = "신규모집기간 시작일", example = "1")
+    Integer newEnrollmentStartDay,
+
+    @Schema(description = "신규모집기간 종료일", example = "10")
+    Integer newEnrollmentEndDay,
+
+    @Schema(description = "재등록기간 시작일", example = "15")
     Integer reEnrollmentStartDate,
+
+    @Schema(description = "재등록기간 종료일", example = "20")
     Integer reEnrollmentEndDate,
+
     @NotNull(message = "images은 null일 수 없습니다.")
     @Size(max = 6, message = "images는 6개 이하여야 합니다.")
+    @Schema(description = "대표 이미지")
     List<String> images,
-    String usageAgreementUrl
+
+    @Schema(description = "이용약관 Path", example = "/swimming-pool/image/1")
+    String usageAgreementPath
 ) {
 
   public UpdateSwimmingPoolBasicInfoCommand toCommand() {
@@ -26,13 +46,12 @@ public record UpdateSwimmingPoolBasicInfoRequest(
         .name(this.name)
         .phone(this.phone)
         .address(this.address)
-        .newEnrollmentStartDate(this.newEnrollmentStartDate)
-        .newEnrollmentEndDate(this.newEnrollmentEndDate)
+        .newEnrollmentStartDate(this.newEnrollmentStartDay)
+        .newEnrollmentEndDate(this.newEnrollmentEndDay)
         .reEnrollmentStartDate(this.reEnrollmentStartDate)
         .reEnrollmentEndDate(this.reEnrollmentEndDate)
         .images(this.images)
-        .usageAgreementUrl(this.usageAgreementUrl)
+        .usageAgreementUrl(this.usageAgreementPath)
         .build();
   }
-
 }
