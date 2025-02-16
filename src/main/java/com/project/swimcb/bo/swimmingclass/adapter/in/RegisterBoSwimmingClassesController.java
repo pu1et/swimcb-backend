11d21d272246e3cfc9db33c1cedc43c404eacc88
pu1et <pu1etproof@gmail.com>
2025-02-16 17:23:14 +0900

    feat: 클래스 데이터 관리 - 클래스 추가 목업 API 생성

---
+++ src/main/java/com/project/swimcb/bo/swimmingclass/adapter/in/RegisterBoSwimmingClassesController.java
@@ -0,0 +1,25 @@
+package com.project.swimcb.bo.swimmingclass.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "BO")
+@RestController
+@RequestMapping("/api/bo/swimming-classes")
+@RequiredArgsConstructor
+public class RegisterBoSwimmingClassesController {
+
+  @Operation(summary = "클래스 데이터 관리 - 클래스 추가")
+  @PostMapping
+  public void registerBoSwimmingClass(
+      @Valid @RequestBody RegisterBoSwimmingClassRequest request
+  ) {
+  }
+}
+
