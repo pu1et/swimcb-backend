8f776574c840cfc37aca4f3439e56f800c74d1b2
pu1et <pu1etproof@gmail.com>
2025-02-17 13:16:43 +0900

    feat: 클래스 데이터 관리 - 강사 리스트 일괄 업데이트 목업 API 생성

---
+++ src/main/java/com/project/swimcb/bo/swimmingclass/adapter/in/UpdateBoSwimmingClassInstructorsController.java
@@ -0,0 +1,25 @@
+package com.project.swimcb.bo.swimmingclass.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "BO")
+@RestController
+@RequestMapping("/api/bo/instructors")
+@RequiredArgsConstructor
+public class UpdateBoSwimmingClassInstructorsController {
+
+  @Operation(summary = "클래스 데이터 관리 - 강사 리스트 일괄 업데이트")
+  @PutMapping
+  public void updateBoSwimmingClassInstructors(
+      @Valid @RequestBody UpdateBoSwimmingClassInstructorsRequest request
+  ) {
+  }
+}
+
