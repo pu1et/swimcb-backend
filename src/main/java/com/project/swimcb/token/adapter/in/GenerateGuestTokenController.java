dd3bc07faec36acfab0383f90ccf5886b5cb29b9
pu1et <pu1etproof@gmail.com>
2025-02-25 10:29:08 +0900

    feat: 회원-고객, 회원-관리자 토큰 생성해주는 목업 API 추가

--- src/main/java/com/project/swimcb/token/adapter/in/GenerateGuestTokenController.java
+++ src/main/java/com/project/swimcb/token/adapter/in/GenerateGuestTokenController.java
@@ -1,5 +1,7 @@
 package com.project.swimcb.token.adapter.in;
 
+import com.project.swimcb.token.application.in.GenerateAdminTokenUseCase;
+import com.project.swimcb.token.application.in.GenerateCustomerTokenUseCase;
 import com.project.swimcb.token.application.in.GenerateGuestTokenUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,15 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "토큰")
 @RestController
-@RequestMapping("/api/token/guest")
+@RequestMapping("/api/token")
 @RequiredArgsConstructor
 public class GenerateGuestTokenController {
 
   private final GenerateGuestTokenUseCase generateGuestTokenUseCase;
+  private final GenerateCustomerTokenUseCase generateCustomerTokenUseCase;
+  private final GenerateAdminTokenUseCase generateAdminTokenUseCase;
 
   @Operation(summary = "게스트 토큰 생성")
-  @PostMapping
+  @PostMapping("/guest")
   public String generateGuestToken() {
     return generateGuestTokenUseCase.generateGuestToken();
+  }
+
+  @Operation(summary = "회원-고객 토큰 생성")
+  @PostMapping("/customer")
+  public String generateCustomerToken() {
+    return generateCustomerTokenUseCase.generateCustomerToken();
+  }
+
+  @Operation(summary = "회원-관리자 토큰 생성")
+  @PostMapping("/admin")
+  public String generateAdminToken() {
+    return generateAdminTokenUseCase.generateAdminToken();
   }
 }
