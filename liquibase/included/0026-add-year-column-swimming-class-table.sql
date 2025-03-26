73c85e9dc6b93b8b2a0855a1b1576fb2e4cc2baa
pu1et <pu1etproof@gmail.com>
2025-03-26 14:32:56 +0900

    refactor: 수영 클래스 테이블에 year 컬럼 붙이는 sql 내 runAlways 제거

--- liquibase/included/0026-add-year-column-swimming-class-table.sql
+++ liquibase/included/0026-add-year-column-swimming-class-table.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset pu1etproof:add-year-column-swimming-class-table runAlways:true
+--changeset pu1etproof:add-year-column-swimming-class-table
 --comment: 수영 클래스 테이블에 year 컬럼 추가
 
 alter table swimming_class add column year int not null default 2025;
