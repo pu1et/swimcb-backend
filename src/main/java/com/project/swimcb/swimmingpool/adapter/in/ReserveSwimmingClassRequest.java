a35365880c37c8180aa14cb0dee6f8cc13b7abad
pu1et <pu1etproof@gmail.com>
2025-03-28 19:30:23 +0900

    refactor: 클래스 예약 API request에 결제 수단 스키마 추가

--- src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveSwimmingClassRequest.java
+++ src/main/java/com/project/swimcb/swimmingpool/adapter/in/ReserveSwimmingClassRequest.java
@@ -1,11 +1,13 @@
 package com.project.swimcb.swimmingpool.adapter.in;
 
 import com.project.swimcb.swimmingpool.domain.enums.PaymentMethod;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record ReserveSwimmingClassRequest(
     long ticketId,
 
+    @Schema(description = "결제 수단", example = "CASH_ON_SITE|BANK_TRANSFER")
     @NotNull(message = "결제 수단은 null이 될 수 없습니다.")
     PaymentMethod paymentMethod
 ) {
