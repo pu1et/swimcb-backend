dad6c3dbee3abc7f917e87035753e7450dc8367d
pu1et <pu1etproof@gmail.com>
2025-05-30 23:01:08 +0900

    refactor: BO 결제대기시간 24시간 초과 서비스 API 붙이기

--- src/main/java/com/project/swimcb/bo/reservation/adapter/in/AutoCancelReservationController.java
+++ src/main/java/com/project/swimcb/bo/reservation/adapter/in/AutoCancelReservationController.java
@@ -1,10 +1,12 @@
 package com.project.swimcb.bo.reservation.adapter.in;
 
 import com.project.swimcb.bo.reservation.application.port.in.BoAutoCancelReservationsUseCase;
+import com.project.swimcb.token.domain.TokenInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,9 @@ public class AutoCancelReservationController {
 
   @Operation(summary = "[BO] 수영장별 24시간 초과 결제 자동 취소 처리")
   @PostMapping
-  public void autoCancel() {
-//    useCase.cancelPaymentExpiredReservations();
+  public void autoCancel(
+      @AuthenticationPrincipal TokenInfo tokenInfo
+  ) {
+    useCase.cancelPaymentExpiredReservations(tokenInfo.swimmingPoolId());
   }
 }
