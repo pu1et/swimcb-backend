47e3386bd636da6e78cece135ed3d63c3d20c32c
pu1et <pu1etproof@gmail.com>
2025-07-05 01:20:06 +0900

    feat: 자유수영 업데이트 request 추가

---
+++ src/main/java/com/project/swimcb/bo/freeswimming/adapter/in/UpdateBoFreeSwimmingController.java
@@ -0,0 +1,29 @@
+package com.project.swimcb.bo.freeswimming.adapter.in;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "BO")
+@SecurityRequirement(name = "Bearer Authentication")
+@RestController
+@RequestMapping("/api/bo/free-swimming/{freeSwimmingId}")
+@RequiredArgsConstructor
+public class UpdateBoFreeSwimmingController {
+
+  @Operation(summary = "자유수영 데이터 관리 - 자유수영 수정")
+  @PutMapping
+  public void updateFreeSwimmingImage(
+      @PathVariable(value = "freeSwimmingId") Long freeSwimmingId,
+      @Valid @RequestBody UpdateBoFreeSwimmingRequest request
+  ) {
+  }
+
+}
---
+++ src/main/java/com/project/swimcb/bo/freeswimming/adapter/in/UpdateBoFreeSwimmingRequest.java
@@ -0,0 +1,79 @@
+package com.project.swimcb.bo.freeswimming.adapter.in;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.YearMonth;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record UpdateBoFreeSwimmingRequest(
+
+    @NotNull(message = "년/월은 null이 될 수 없습니다.")
+    @Schema(description = "자유수영 년/월", example = "2025-10")
+    YearMonth yearMonth,
+
+    @NotEmpty(message = "자유수영 요일은 필수입니다.")
+    @Schema(description = "자유수영 요일")
+    List<DayOfWeek> days,
+
+    @Valid
+    @NotNull(message = "자유수영 시작/종료 시간은 null이 될 수 없습니다.")
+    @Schema(description = "자유수영 시작/종료 시간")
+    Time time,
+
+    @Schema(description = "안전근무 ID", example = "1")
+    Long lifeguardId,
+
+    @Valid
+    @NotEmpty(message = "자유수영 티켓 리스트는 필수입니다.")
+    @Schema(description = "자유수영 티켓 리스트")
+    List<Ticket> tickets,
+
+    @NotNull(message = "정원 정보는 null이 될 수 없습니다.")
+    @Min(value = 0, message = "정원 정보는 0 이상이어야 합니다.")
+    @Schema(description = "정원 정보")
+    Integer capacity,
+
+    @NotNull(message = "사용자 노출 여부는 null이 될 수 없습니다.")
+    @Schema(description = "사용자 노출 여부", example = "true")
+    Boolean isExposed
+) {
+
+  @Builder
+  @Schema(name = "UpdateBoFreeSwimmingRequest.Time")
+  record Time(
+
+      @NotNull(message = "자유수영 시작 시간은 null이 될 수 없습니다.")
+      @Schema(description = "자유수영 시작 시간", example = "06:00")
+      LocalTime startTime,
+
+      @NotNull(message = "자유수영 종료 시간은 null이 될 수 없습니다.")
+      @Schema(description = "자유수영 종료 시간", example = "06:50")
+      LocalTime endTime
+  ) {
+
+  }
+
+  @Builder
+  @Schema(name = "UpdateBoFreeSwimmingRequest.Ticket")
+  record Ticket(
+
+      @NotEmpty(message = "티켓 이름은 필수입니다.")
+      @Schema(description = "티켓 이름", example = "성인일반")
+      String name,
+
+      @NotNull(message = "티켓 가격은 null이 될 수 없습니다.")
+      @Min(value = 0, message = "티켓 가격은 0 이상이어야 합니다.")
+      @Schema(description = "티켓 가격", example = "100000")
+      Integer price
+  ) {
+
+  }
+
+}
