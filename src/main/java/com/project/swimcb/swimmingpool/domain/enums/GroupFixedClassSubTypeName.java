64e12e746dea08cc971404d4daa6da5fa47441f6
pu1et <pu1etproof@gmail.com>
2025-07-25 20:42:03 +0900

    refactor: 강습구분 추가

--- src/main/java/com/project/swimcb/swimmingpool/domain/enums/GroupFixedClassSubTypeName.java
+++ src/main/java/com/project/swimcb/swimmingpool/domain/enums/GroupFixedClassSubTypeName.java
@@ -9,7 +9,9 @@ public enum GroupFixedClassSubTypeName {
   BASIC("기초"),
   BEGINNER("초급"),
   INTERMEDIATE("중급"),
-  ADVANCED("고급"),
+  BEGINNER_INTERMEDIATE("초중급"),
+  INTERMEDIATE_ADVANCED("중상급"),
+  ADVANCED("상급"),
   TRAINING("연수"),
   MASTERS("마스터스");
 
