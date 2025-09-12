a7ccf3b6707a92fae7fe528e29f094bd8d0345e3
pu1et <pu1etproof@gmail.com>
2025-09-12 15:53:52 +0900

    feat: 애플 로그인 콜백 API 추가

--- src/main/java/com/project/swimcb/oauth2/adapter/in/OAuth2CallbackController.java
+++ src/main/java/com/project/swimcb/oauth2/adapter/in/OAuth2CallbackController.java
@@ -5,25 +5,30 @@ import com.project.swimcb.oauth2.domain.enums.Environment;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+@Slf4j
 @Tag(name = "회원")
 @SecurityRequirement(name = "Bearer Authentication")
 @RestController
-@RequestMapping("/login/oauth2/kakao")
+@RequestMapping("/login/oauth2")
 @RequiredArgsConstructor
 public class OAuth2CallbackController {
 
   private final OAuth2Adapter oAuth2Adapter;
 
   @Operation(summary = "카카오 콜백 처리")
-  @GetMapping
-  public RedirectView success(
+  @GetMapping("/kakao")
+  public RedirectView kakaoSuccess(
       @RequestParam(value = "code") String code,
       @RequestParam(value = "state", required = false) String state
   ) {
@@ -36,10 +41,50 @@ public class OAuth2CallbackController {
     ).redirectView();
   }
 
+  @Operation(summary = "애플 콜백 처리")
+  @PostMapping("/apple")
+  public RedirectView appleSuccess(
+      @RequestBody AppleAuthorizationResponse response
+  ) {
+
+    log.info(response.toString());
+    return null;
+  }
+
   private Environment env(String state) {
     if (state == null || state.isBlank()) {
       return null;
     }
     return Environment.valueOf(state.toUpperCase());
   }
+
+  private record AppleAuthorizationResponse(
+      @NonNull Authorization authorization,
+      @NonNull User user
+  ) {
+
+    private record Authorization(
+        @NonNull String code,
+        @NonNull String idToken,
+        @NonNull String state
+    ) {
+
+    }
+
+    private record User(
+        @NonNull Name name,
+        @NonNull String email
+    ) {
+
+    }
+
+    private record Name(
+        @NonNull String firstName,
+        @NonNull String lastName
+    ) {
+
+    }
+
+  }
+
 }
